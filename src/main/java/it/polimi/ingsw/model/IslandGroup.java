4a0421eb41477824344605d3a2ff1cce4cadce08
Carlo Sgaravatti <carlo.sgaravatti@gmail.com>
2022-03-13 17:59:17 +0100

    Update IslandGroup and SingleIsland

--- src/main/java/it/polimi/ingsw/model/IslandGroup.java
+++ src/main/java/it/polimi/ingsw/model/IslandGroup.java
@@ -1,14 +1,34 @@
 package it.polimi.ingsw.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class IslandGroup extends Island {
-	private Island[] islands;
+	private final List<Island> islands;
 
-	public void createIslandGroup(Island i1, Island i2) {
-
+	public IslandGroup(Island i1, Island i2) {
+		islands = new ArrayList<>();
+		addIsland(i1);
+		addIsland(i2);
 	}
 
 	public void addIsland(Island i) {
+		islands.add(i);
+	}
 
+	@Override
+	public int getInfluence (Player p) {
+		int res = 0;
+		for (Island i: islands)
+			res += i.getInfluence(p);
+		return res;
+	}
+
+	@Override
+	public void putTower(TowerType towerType) {
+		for (Island i: islands)
+			if (i.getTowerType() == null || i.getTowerType() != towerType)
+				i.putTower(towerType);
 	}
 
 }
--- src/main/java/it/polimi/ingsw/model/SingleIsland.java
+++ src/main/java/it/polimi/ingsw/model/SingleIsland.java
@@ -1,5 +1,13 @@
 package it.polimi.ingsw.model;
 
 public class SingleIsland extends Island {
+    @Override
+    public int getInfluence(Player p) {
+        return 0;
+    }
 
+    @Override
+    public void putTower(TowerType t){
+
+    }
 }
