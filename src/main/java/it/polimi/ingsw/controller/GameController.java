7559c450139ce3bb531a6b68e9dd2b0fe3254b70
Carlo Sgaravatti <carlo.sgaravatti@gmail.com>
2022-04-05 14:50:06 +0200

    GameController update

--- src/main/java/it/polimi/ingsw/controller/GameController.java
+++ src/main/java/it/polimi/ingsw/controller/GameController.java
@@ -1,13 +1,17 @@
 package it.polimi.ingsw.controller;
 
 import it.polimi.ingsw.model.Game;
+import it.polimi.ingsw.model.Player;
 
 public class GameController {
 	private TurnController turnController;
 	private ActionController actionController;
 	private InitController initController;
-
 	private Game game;
+
+	public GameController() {
+		initController = new InitController();
+	}
 
 	public void startGame() {
 		game.start();
@@ -33,8 +37,20 @@ public class GameController {
 		return actionController;
 	}
 
+	public InitController getInitController() {
+		return initController;
+	}
+
 	public void setGame(){
-		this.game=initController.getGame();
+		this.game = initController.getGame();
+	}
+
+	public void setGame(Game game) {
+		this.game = game;
+	}
+
+	public void initializeTurnController() {
+		turnController = new TurnController(game.getPlayers().toArray(new Player[0]), game);
 	}
 
 }
